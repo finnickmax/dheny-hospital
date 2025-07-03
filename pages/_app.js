1848ac805ebbe6e46cf934fe7f9664e0169e447e
finnickmax <hanariguriko@gmail.com>
2025-07-03 15:24:23 +0700

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import { PatientProvider } from '@/context/PatientContext';
+import { InpatientProvider } from '@/context/InpatientContext';
+import '@/styles/globals.css';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <PatientProvider>
+      <InpatientProvider>
+        <Component {...pageProps} />
+      </InpatientProvider>
+    </PatientProvider>
+  );
+}
