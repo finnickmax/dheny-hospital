4e8e930d245bd5adfba13c978ccd4c6981760ee4
finnickmax <hanariguriko@gmail.com>
2025-07-03 14:20:05 +0700

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,8 @@
+import Layout from '../components/Layout';
+
 export default function Dashboard() {
   return (
-    <div style={{ padding: 40 }}>
+    <Layout>
       <h1>Dashboard Utama - Dheny Hospital SIMRS</h1>
       <p>Selamat datang di halaman dashboard. Pilih modul di bawah:</p>
       <ul>
@@ -8,6 +10,6 @@ export default function Dashboard() {
         <li><a href="/rawatjalan">Modul Rawat Jalan</a></li>
         <li><a href="/integrasi">Integrasi BPJS & SATUSEHAT</a></li>
       </ul>
-    </div>
+    </Layout>
   );
 }
