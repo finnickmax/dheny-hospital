ce9f48cf52a6f2a19a4be78095c40e804923b0bb
finnickmax <hanariguriko@gmail.com>
2025-07-03 14:10:19 +0700

    Create pendaftaran.js

---
+++ pages/pendaftaran.js
@@ -0,0 +1,8 @@
+export default function Pendaftaran() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Modul Pendaftaran</h1>
+      <p>Halaman ini menampilkan simulasi pendaftaran pasien baru, pencarian pasien lama, dan manajemen antrian.</p>
+    </div>
+  );
+}
