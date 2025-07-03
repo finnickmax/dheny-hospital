efe68e32078480142c4a44482a407ab00d20882f
finnickmax <hanariguriko@gmail.com>
2025-07-03 14:11:17 +0700

    Create integrasi.js

---
+++ pages/integrasi.js
@@ -0,0 +1,8 @@
+export default function Integrasi() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Integrasi BPJS & SATUSEHAT</h1>
+      <p>Halaman ini menampilkan simulasi integrasi API VClaim BPJS dan SATUSEHAT Kemenkes.</p>
+    </div>
+  );
+}
