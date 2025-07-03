d758ac9f168eff85c3b13d0ffca50b4297ccdfb0
finnickmax <hanariguriko@gmail.com>
2025-07-03 13:50:12 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Dheny Hospital - SIMRS Demo
+
+## How to Run Locally
+1. Run `npm install`
+2. Run `npm run dev`
+3. Open `http://localhost:3000`
+
+## Login Demo
+- Username: demo
+- Password: demo123
---
+++ dashboard.js
@@ -0,0 +1,13 @@
+export default function Dashboard() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Dheny Hospital Dashboard</h1>
+      <ul>
+        <li>Rawat Jalan</li>
+        <li>Farmasi</li>
+        <li>Billing</li>
+        <li>Rekam Medis</li>
+      </ul>
+    </div>
+  )
+}
