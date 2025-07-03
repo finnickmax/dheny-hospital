0e2bca76713a114ef44d7ce182f94d74a1b7e8cd
finnickmax <hanariguriko@gmail.com>
2025-07-03 14:10:43 +0700

    Create rawatjalan.js

---
+++ pages/rawatjalan.js
@@ -0,0 +1,8 @@
+export default function RawatJalan() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Modul Rawat Jalan</h1>
+      <p>Halaman ini menampilkan fitur rawat jalan seperti pemeriksaan dokter, rekam medis, dan resep obat.</p>
+    </div>
+  );
+}
